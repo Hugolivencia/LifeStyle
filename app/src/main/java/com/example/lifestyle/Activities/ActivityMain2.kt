8cc4c7c29dca27f6d3b412ba5159447ff86db282
holiv <holivenciachiner@gmail.com>
2024-05-28 20:30:38 +0200

    cambios sobre las imagenes

--- app/src/main/java/com/example/lifestyle/Activities/ActivityMain2.kt
+++ app/src/main/java/com/example/lifestyle/Activities/ActivityMain2.kt
@@ -1,182 +1,133 @@
 package com.example.lifestyle.Activities
 
-
-import android.content.ContentValues.TAG
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.lifestyle.Activities.login.ActivityRegister
 import com.example.lifestyle.R
 import com.example.lifestyle.Recipe
 import com.example.lifestyle.databinding.ActivityMain2Binding
 import com.google.android.gms.tasks.Task
 import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
-
 
 open class ActivityMain2 : AppCompatActivity() {
 
     private lateinit var binding: ActivityMain2Binding
     private val db = FirebaseFirestore.getInstance()
-    val uri: Uri? = null
-    val view: View? = null
-    var firebaseDatabase: FirebaseDatabase? = null
-    var firebaseStorage: FirebaseStorage? = null
-    var imageUri: Uri? = null // Declaración de la variable para almacenar la URI de la imagen
-    var idFireStore: String? = null
+    private lateinit var firebaseStorage: FirebaseStorage
+    private var imageUri: Uri? = null
+    private var idFireStore: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMain2Binding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
-        // Inicializar la base de datos
+        // Inicializar Firebase
         FirebaseApp.initializeApp(this)
+        firebaseStorage = FirebaseStorage.getInstance()
 
-        // Inicializar el storage para guardar las imagenes
-        FirebaseStorage.getInstance()
-        subirimage()
         binding.imageButton.setOnClickListener {
-
             startActivity(Intent(applicationContext, MainActivity::class.java))
-
         }
-
 
         binding.saveButton.setOnClickListener {
-            val ingredientes: MutableList<String> = mutableListOf()
-            val pasosPreparacion: MutableList<String> = mutableListOf()
-
-            for(i in 0 until binding.linearlayout2.childCount){
-                val view = binding.linearlayout2.getChildAt(i)
-                if(view is EditText){
-                    val ingrediente = view.text.toString()
-                    ingredientes.add(ingrediente)
-                    guardarIngrediente(ingredientes!!)
-                }
-            }
-
-            for(i in 0 until binding.linearlayout3.childCount){
-                val view2 = binding.linearlayout3.getChildAt(i)
-                if(view2 is EditText){
-                    val pasoPreparacion = view2.text.toString()
-                    pasosPreparacion.add(pasoPreparacion)
-                    guardarPasos(pasosPreparacion!!)
-                }
-            }
-
-            // Crear un objeto Recipe con los datos ingresados y la URI de la imagen
-            val newRecipe = Recipe(
-                idFireStore,
-                recuperarUsuario(),
-                binding.editTextRecipeName.text.toString(),
-                binding.editTextDuration.text.toString(),
-                binding.editTextCalories.text.toString(),
-                ingredientes,
-                binding.editTextDescription.text.toString(),
-                imageUri.toString(),
-                pasosPreparacion
-            )
-
-
-
-            if(binding.editTextRecipeName.text.toString().isEmpty()){
-                Toast.makeText(this,"El campo de nombre es obligatorio",Toast.LENGTH_SHORT).show()
-            }else {
-                // Subir los datos a Firestore
-                val db = FirebaseFirestore.getInstance()
-                db.collection("recipes").add(newRecipe)
-                    .addOnSuccessListener { documentReference ->
-
-                        // Obtenemos la id generada por firStore
-                        idFireStore = documentReference.id
-
-                        // Actualizamos el documentos para agregar la id como campo del documento
-                        documentReference.update("id", idFireStore)
-                            .addOnSuccessListener {
-                                Log.d(TAG, "ID almacenado como un campo dentro del documento.")
-                            }
-                            .addOnFailureListener { e ->
-                                Log.w(TAG, "Error al actualizar el campo 'id' dentro del documento.", e)
-                            }
-
-                        // Mostrar un mensaje de éxito o realizar otras acciones si es necesario
-                        Toast.makeText(this, "Receta guardada correctamente", Toast.LENGTH_SHORT).show()
-                    }
-                    .addOnFailureListener { e ->
-                        // Mostrar un mensaje de error si falla la subida
-                        Toast.makeText(
-                            this,
-                            "Error al guardar la receta: ${e.message}",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
-
-                startActivity(Intent(applicationContext, MainActivity::class.java))
-            }
-
-
+            saveRecipe()
         }
-
 
         binding.imageViewRecipe.setOnClickListener {
-            // Crear un intent para abrir la galería
             val intent = Intent(Intent.ACTION_PICK)
-            intent.type = "image/*" // Filtrar solo imágenes
-            startActivityForResult(intent, PICK_IMAGE_REQUEST) // Iniciar la actividad de la galería
+            intent.type = "image/*"
+            startActivityForResult(intent, PICK_IMAGE_REQUEST)
         }
 
-
-        // Crear un nuevo text view
         binding.CrearTextView.setOnClickListener {
-            // Crea un nuevo EditText programáticamente
-            val newEditText = EditText(this)
-
-            newEditText.id = View.generateViewId()  // Asignar un ID único al EditText
-            newEditText.hint = "Escribe un nuevo ingrediente..."
-            //newEditText.setBackgroundResource(R.drawable.edit_text_border) // Establecer un estilo
-            newEditText.height = 110
-            newEditText.textSize = 10f // Tamaño del texto
-            newEditText.setPadding(10, 0, 0, 0) // Añadir el padding
-            newEditText.setPaddingRelative(10, 10, 0, 10)
-            // Agrega el EditText al LinearLayout
-            binding.linearlayout2.addView(newEditText)
+            addNewEditText(binding.linearlayout2, "Escribe un nuevo ingrediente...")
         }
 
-        // Crear un nuevo text view
         binding.CrearText.setOnClickListener {
-            // Crea un nuevo EditText programáticamente
-            val nuevoEditText = EditText(this)
+            addNewEditText(binding.linearlayout3, "Escribe un nuevo paso de preparación...")
+        }
+    }
 
-            nuevoEditText.id = View.generateViewId()  // Asignar un ID único al EditText
-            nuevoEditText.hint = "Escribe un nuevo paso de preparación..."
-            //nuevoEditText.setBackgroundResource(R.drawable.edit_text_border) // Establecer un estilo
-            nuevoEditText.height = 110
-            nuevoEditText.textSize = 10f // Tamaño del texto
-            nuevoEditText.setPadding(10, 0, 0, 0) // Añadir el padding
-            nuevoEditText.setPaddingRelative(10, 10, 0, 10)
+    private fun addNewEditText(layout: ViewGroup, hint: String) {
+        val newEditText = EditText(this)
+        newEditText.id = View.generateViewId()
+        newEditText.hint = hint
+        newEditText.height = 110
+        newEditText.textSize = 10f
+        newEditText.setPadding(10, 0, 0, 0)
+        newEditText.setPaddingRelative(10, 10, 0, 10)
+        layout.addView(newEditText)
+    }
 
-            // Agrega el EditText al LinearLayout
-            binding.linearlayout3.addView(nuevoEditText)
+    private fun saveRecipe() {
+        val ingredientes: MutableList<String> = mutableListOf()
+        val pasosPreparacion: MutableList<String> = mutableListOf()
+
+        for (i in 0 until binding.linearlayout2.childCount) {
+            val view = binding.linearlayout2.getChildAt(i)
+            if (view is EditText) {
+                val ingrediente = view.text.toString()
+                ingredientes.add(ingrediente)
+                guardarIngrediente(ingredientes)
+            }
+        }
+
+        for (i in 0 until binding.linearlayout3.childCount) {
+            val view2 = binding.linearlayout3.getChildAt(i)
+            if (view2 is EditText) {
+                val pasoPreparacion = view2.text.toString()
+                pasosPreparacion.add(pasoPreparacion)
+                guardarPasos(pasosPreparacion)
+            }
+        }
+
+        val newRecipe = Recipe(
+            idFireStore,
+            recuperarUsuario(),
+            binding.editTextRecipeName.text.toString(),
+            binding.editTextDuration.text.toString(),
+            binding.editTextCalories.text.toString(),
+            ingredientes,
+            binding.editTextDescription.text.toString(),
+            imageUri?.toString() ?: "",
+            pasosPreparacion
+        )
+
+        if (binding.editTextRecipeName.text.toString().isEmpty()) {
+            Toast.makeText(this, "El campo de nombre es obligatorio", Toast.LENGTH_SHORT).show()
+        } else {
+            db.collection("recipes").add(newRecipe)
+                .addOnSuccessListener { documentReference ->
+                    idFireStore = documentReference.id
+                    documentReference.update("id", idFireStore)
+                        .addOnSuccessListener {
+                            Log.d("TAG", "ID almacenado como un campo dentro del documento.")
+                        }
+                        .addOnFailureListener { e ->
+                            Log.w("TAG", "Error al actualizar el campo 'id' dentro del documento.", e)
+                        }
+                    Toast.makeText(this, "Receta guardada correctamente", Toast.LENGTH_SHORT).show()
+                }
+                .addOnFailureListener { e ->
+                    Toast.makeText(this, "Error al guardar la receta: ${e.message}", Toast.LENGTH_SHORT).show()
+                }
+            startActivity(Intent(applicationContext, MainActivity::class.java))
         }
     }
 
     private fun guardarIngrediente(ingrediente: MutableList<String>) {
-
         val db = db.collection("recipes").document(binding.editTextRecipeName.text.toString())
-
         val updateTask: Task<Void> = db.update("ingredientes", ingrediente)
-
-        // Maneja el resultado de la actualización
         updateTask.addOnCompleteListener { task ->
             if (task.isSuccessful) {
                 println("Lista agregada correctamente al campo existente.")
@@ -184,31 +135,22 @@ open class ActivityMain2 : AppCompatActivity() {
                 println("Error al agregar la lista al campo existente: ${task.exception?.message}")
             }
         }
-
     }
 
     private fun recuperarUsuario(): String? {
         val auth = FirebaseAuth.getInstance()
         val usuarioActual = auth.currentUser
-        var nombreUsuario: String? = null  // Inicializar la variable fuera del bloque if
-
+        var nombreUsuario: String? = null
         if (usuarioActual != null) {
-            // Obtener el nombre de usuario del usuario autentificado por Firebase
             nombreUsuario = usuarioActual.displayName
         }
-
         Log.d("ActivityRegister", "UsuarioActivity10: $nombreUsuario")
         return nombreUsuario
     }
 
-
     private fun guardarPasos(pasoPreparacion: MutableList<String>?) {
-
         val db = db.collection("recipes").document(binding.editTextRecipeName.text.toString())
-
         val updateTask: Task<Void> = db.update("pasosPreparacion", pasoPreparacion)
-
-        // Maneja el resultado de la actualización
         updateTask.addOnCompleteListener { task ->
             if (task.isSuccessful) {
                 println("Lista agregada correctamente al campo existente.")
@@ -216,41 +158,53 @@ open class ActivityMain2 : AppCompatActivity() {
                 println("Error al agregar la lista al campo existente: ${task.exception?.message}")
             }
         }
-
     }
 
-    private fun subirimage() {
-        val reference = firebaseStorage!!.reference.child("img")
-            .child(System.currentTimeMillis().toString() + "")
-        reference.putFile(uri!!).addOnSuccessListener {
-            reference.downloadUrl.addOnSuccessListener { uri ->
-                val recipe = Recipe()
-                recipe.img = uri.toString()
-                firebaseDatabase!!.reference.child("Imagenes").push()
-                    .setValue(recipe).addOnSuccessListener {
-                        finish()
-                    }
-                    .addOnFailureListener {
-                        Toast.makeText(this, "Error al Subir Imagen...", Toast.LENGTH_SHORT).show()
-                    }
-            }
-        }
-    }
-    // Código de solicitud para seleccionar una imagen de la galería
-    private val PICK_IMAGE_REQUEST = 1
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null) {
-            // La imagen se ha seleccionado correctamente desde la galería
             imageUri = data.data
-            // Ahora puedes utilizar la Uri de la imagen como desees
-            // Por ejemplo, mostrarla en un ImageView
             binding.imageViewRecipe.setImageURI(imageUri)
+            subirImageAlStorege(imageUri)
         }
     }
 
+    private fun subirImageAlStorege(imageUri: Uri?) {
+        if (imageUri != null) {
+            // Crea una referencia en Firebase Storage
+            val storageReference = firebaseStorage.reference.child("img/${System.currentTimeMillis()}.jpg")
 
+            // Sube el archivo a Firebase Storage
+            storageReference.putFile(imageUri)
+                .addOnSuccessListener { taskSnapshot ->
+                    // Obtén la URL de descarga pública del archivo subido
+                    storageReference.downloadUrl.addOnSuccessListener { uri ->
+                        val downloadUrl = uri.toString()
+                        // Guarda la URL en Realtime Database o Firestore
+                        guardarImagen(downloadUrl)
+                    }
+                }
+                .addOnFailureListener { e ->
+                    Log.e("Firebase", "Upload failed", e)
+                }
+        }
+    }
 
+    private fun guardarImagen(downloadUrl: String) {
+        val imageInfo = hashMapOf(
+            "url" to downloadUrl,
+            "timestamp" to System.currentTimeMillis()
+        )
+        db.collection("images").add(imageInfo)
+            .addOnSuccessListener {
+                Toast.makeText(this, "URL de la imagen guardada en la base de datos", Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener { e ->
+                Toast.makeText(this, "Error al guardar la URL en la base de datos: ${e.message}", Toast.LENGTH_SHORT).show()
+            }
+    }
 
-
+    companion object {
+        private const val PICK_IMAGE_REQUEST = 1
+    }
 }
